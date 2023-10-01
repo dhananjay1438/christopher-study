df98499250f859971da1eade584b0013b0dfeed1
Dhananjay <dhananjaypanage11@gmail.com>
2023-10-01 22:39:55 +0530

    Added js

---
+++ js/variables.js
@@ -0,0 +1,33 @@
+// Variable is a container which stores values
+
+//password
+// characters + numbers
+// Special symbols
+// true or false
+
+
+// label = name
+
+// container name
+// var = variable
+// String = characters + numbers + special symbols 
+// String always have double quotes around it
+var password =  "Password@123"
+// var conatiner/variable_name
+
+
+// This is a number
+var num = 10
+
+// This is a boolean (true or false)
+var is_raining = true
+var school_time = false
+
+// Double
+var number_with_point = 34.95
+
+// String
+var name = "Christopher"
+var age = "11" 
+var number_age = 11
+// Whatever is written inside double quotes is considered as a string
---
+++ js/variables_assignment.js
@@ -0,0 +1,26 @@
+
+// What is this? String, number or boolean?
+var a = "hello world"
+
+var b = "@!@#"
+var c = "password@123"
+
+var some = "1234"
+var some2 = 1234
+
+var is_boy = true
+
+var num1 = 10
+var num2 = 20
+
+console.log(num1+num2)
+console.log(10+20)
+
+console.log(num2 - num1)
+console.log(20 - 10)
+
+console.log(num1 * num2)
+console.log(10*20)
+
+console.log(num2 / num1)
+console.log(20/10)
