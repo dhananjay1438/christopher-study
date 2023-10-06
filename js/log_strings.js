21dec59a239dbbb88ed0ddeb2d29484d187855bd
Dhananjay <dhananjaypanage11@gmail.com>
2023-10-07 00:21:33 +0530

    07 Oct

---
+++ js/log_strings.js
@@ -0,0 +1,13 @@
+var a = 10
+var b = 20
+
+
+console.log(a)
+console.log(b)
+
+console.log("Addition", a+b)
+console.log("Substraction", a-b)
+
+var name = "Christopher"
+
+console.log("Hello", name)
