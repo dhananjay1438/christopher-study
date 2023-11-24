54b6afd3093b2709315e020e12a08995deba3606
Dhananjay Panage <dhananjaypanage11@gmail.com>
2023-11-25 00:21:58 +0530

    Added python and some js files

---
+++ python/variables.py
@@ -0,0 +1,27 @@
+# Integers
+a = 10
+b = 20
+
+# Booleans (True or False)
+is_night = False
+is_day = True
+
+# strings
+string = "hello this is a string"
+
+# double or float
+pi = 3.14
+
+print(a)
+print(b)
+
+
+# Perform some basic operation
+
+sum = a + b
+print(sum)
+
+multi = a * b
+print(multi)
+
+x = 20
